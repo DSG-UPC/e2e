35ebb4103a8567c787fdbbb2460d102161e98789
adrian-manco <adrian.manco@estudiantat.upc.edu>
2021-04-01 14:15:06 +0200

    .

--- src/test0.js
+++ src/test0.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg')
 const { exec } = require('child_process')
 
 
-async function test() {
+async function test0() {
     const pool = new Pool({
         host: "localhost",
         port: 5432,
@@ -39,49 +39,8 @@ async function test() {
     for (i = 5; i < 9; ++i) {
         console.log(`           ${accounts[i]}  ${await tok.methods.balanceOf(accounts[i]).call({ from: accounts[0] })} tokens`)
     }
+    console.log(`TEST 0: IDEAL SCENARIO`)
 
-    /*     await pool.query(
-            `\
-            drop table subs cascade;
-            drop table meds cascade;
-            drop table provs cascade;
-            drop table subscriptions cascade;
-    
-            create table if not exists subs
-            (sub varchar not null,
-            primary key (sub));
-            
-            create table if not exists meds
-            (med varchar not null,
-            primary key (med));
-            
-            create table if not exists provs
-            (prov varchar not null,
-            primary key (prov));
-    
-            create table if not exists subscriptions
-            (sub varchar not null, med varchar not null, prov varchar not null, lim integer not null, charge integer not null, unit varchar not null, num varchar not null, fwd boolean not null, active boolean not null,
-            primary key (sub, med, prov),
-            foreign key (sub) references subs (sub),
-            foreign key (med) references meds (med),
-            foreign key (prov) references provs (prov));`, async (err, result) => {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                console.log(`Database initialized succesfully. Inserting test accounts...`)
-    
-                await pool.query(`\
-                insert into subs (sub) values ('${accounts[1]}'), ('${accounts[2]}'), \
-                ('${accounts[3]}'), ('${accounts[4]}');\
-                insert into meds (med) values ('${medAt}');\
-                insert into provs (prov) values ('${accounts[5]}'), ('${accounts[6]}'), \
-                ('${accounts[7]}'), ('${accounts[8]}');`, (err, result) => {
-                    if (err) { console.log(`${err}\nAccounts not added.`) }
-                    else { console.log(`Test accounts added to the database. Ready to perform tests.`) }
-                }, (err, result) => {return})
-            }
-        }, (err, result) => {return}) */
     await pool.query(
         `\
         drop table subs cascade;
@@ -154,6 +113,38 @@ async function test() {
     `)
     console.log(`Subscriptions marked as active. Token transfers can begin.`)
 
+    /* Step 4: Provider has to specify the amount to charge for each subscription. This amount is valid only for the
+    next payment. The provider has to specify the amount to charge to each subscriptor before the mediator can claim the deposit. */
+    await pool.query(`\
+    update subscriptions set charge = 5 where\
+    sub = '${accounts[1]}' and med = '${medAt}' and prov = '${accounts[5]}';
+
+    update subscriptions set charge = 10 where\
+    sub = '${accounts[2]}' and med = '${medAt}' and prov = '${accounts[6]}';
+
+    update subscriptions set charge = 15 where\
+    sub = '${accounts[3]}' and med = '${medAt}' and prov = '${accounts[7]}';
+
+    update subscriptions set charge = 20 where\
+    sub = '${accounts[4]}' and med = '${medAt}' and prov = '${accounts[8]}';    
+    `)
+    console.log(`Providers have set the amount to charge for each subscriptor they are related with.`)
+
+    /* Step 5: At this point, the subscribers can manually deposit the required amount in the mediator, or the mediator can pull the funds ou of the subscriber account (hence the previous approval). */
+    console.log(`Starting movement of tokens towards the mediator.`)
+    var result
+    for (i=1; i<5; ++i){
+        result = await pool.query(`select charge from subscriptions where sub = '${accounts[i]}' and med = '${medAt}' and prov = '${accounts[i+4]}'`)
+        await med.methods.depositPull(accounts[i], accounts[i+4], parseInt(result.rows[0].charge)).send({ from: accounts[0] }) 
+        console.log(`           ${accounts[i]} now has ${await tok.methods.balanceOf(accounts[i]).call({ from: accounts[0] })} tokens.`)
+    }
+   /*  for (i=1; i<5; ++i){
+        result = await pool.query(`select charge from subscriptions where sub = '${accounts[i]}' and med = '${medAt}' and prov = '${accounts[i+4]}'`)
+        await med.methods.depositPush(accounts[i+4], parseInt(result.rows[0].charge).send({ from: accounts[i] }))
+        console.log(`           ${accounts[i]} now has ${await tok.methods.balanceOf(accounts[i]).call({ from: accounts[0] })} tokens.`)
+    } */
+    console.log(`Mediator has ${await tok.methods.balanceOf(medAt).call({ from: accounts[0] })} tokens from subscriptors.`)
+
     await pool.end()
 }
-test()
+test0()
